6cf54df9786a802734db55e20f5ee3d9bdae9171
a-a-tel <anouaranthonytel@gmail.com>
2025-04-30 05:11:09 +0200

    template finished

--- bot/src/main/java/com/anthony/discordKagan/Main.java
+++ bot/src/main/java/com/anthony/discordKagan/Main.java
@@ -1,7 +1,32 @@
 package com.anthony.discordKagan;
 
-public class Main {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+public class Main extends ListenerAdapter {
+
+    public static JDA jda = JDABuilder.createDefault(Token.BOT_TOKEN)
+            .setMemberCachePolicy(MemberCachePolicy.ALL)
+            .enableIntents(
+                    GatewayIntent.GUILD_MEMBERS,
+                    GatewayIntent.MESSAGE_CONTENT,
+                    GatewayIntent.GUILD_MESSAGE_POLLS
+            )
+            .addEventListeners(new Main()).build();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Loading Bot...");
     }
+
+    /// Begin app listener ///
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+    }
+
 }
